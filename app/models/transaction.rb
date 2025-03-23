02ab300426b575b8bc43ab8ab150c8c6c38da84f
Vadivelan Kuppusamy <reachme.vadivelan@gmail.com>
2025-03-23 11:50:34 +0800

    Update validation for Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,8 @@ class Transaction < ApplicationRecord
   belongs_to :wallet
   belongs_to :sender, class_name: 'User', foreign_key: :sender_id
 
+  TRANSACTION_TYPES = %w[credit debit transfer].freeze
+
   validates_presence_of :transaction_type, :sender_id, :amount, :status
+  validates_inclusion_of :transaction_type, in: TRANSACTION_TYPES
 end
