df1c279432c120248a518c6797475b5a910bc37f
Vadivelan Kuppusamy <reachme.vadivelan@gmail.com>
2025-03-23 11:50:14 +0800

    Seed users with wallet setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   has_many :user_transactions, class_name: 'Transaction', foreign_key: :sender_id
 
   validates_presence_of :name, :email, :phone
+
+  after_create  :setup_wallet
+
+  DEFAULT_WALLET_TYPE = 'credit_card'.freeze
+
+  private
+  def setup_wallet
+    create_wallet(wallet_type: DEFAULT_WALLET_TYPE)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times.each do |number|
+  index = number + 1
+  user = User.find_or_initialize_by(phone: "9090909#{index}")
+  user.assign_attributes(name: "User_#{index}", email: "user_#{index}@gmail.com")
+  user.save
+  puts "User saved: #{user.name}"
+end
