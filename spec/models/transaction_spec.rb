edfe29eab2f4559bfb33e7a53674562be8f5f424
Vadivelan Kuppusamy <reachme.vadivelan@gmail.com>
2025-03-28 16:21:34 +0800

    Cover spec for transaction model

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe Transaction, type: :model do
   it do
     expect(subject).to be_valid
   end
+
+  context 'associations' do
+    it { should belong_to(:wallet) }
+    it { should belong_to(:perform_by).class_name('User') }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of(:transaction_type) }
+    it { should validate_inclusion_of(:transaction_type).in_array(Transaction::TRANSACTION_TYPES) }
+    it { should validate_presence_of(:amount) }
+    it { should validate_numericality_of(:amount) }
+    it { should validate_presence_of(:status) }
+  end
+
+  describe '#mark_as_completed' do
+    it do
+      subject.mark_as_completed
+
+      expect(subject.status).to eql('completed')
+    end
+  end
 end
